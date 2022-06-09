d38a61df23336ea8b5e1d7384aad6259db964ee0
deployn <54767886+deployn@users.noreply.github.com>
2022-06-09 11:48:48 +0200

    Added missing port variable in the entrypoint scripts (#62)

--- files/akaunting-php-fpm-nginx-supervisord.sh
+++ files/akaunting-php-fpm-nginx-supervisord.sh
@@ -9,6 +9,7 @@ if [ "$AKAUNTING_SETUP" == "true" ]; then
     while sleep $retry_interval; do
         if php artisan install \
             --db-host=$DB_HOST \
+            --db-port=$DB_PORT \
             --db-name=$DB_DATABASE \
             --db-username=$DB_USERNAME \
             "--db-password=$DB_PASSWORD" \
